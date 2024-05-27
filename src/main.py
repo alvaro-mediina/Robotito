b49ec553ddd1a51214784e3a2064c64821c5f25e
Santi <alvaro.mediina2003@gmail.com>
2024-05-27 12:35:37 -0300

    main

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("../utils/")
+sys.path.append("../movement/")
+import time
+
+
+from motor import Motor
+#from Mpu6060 import Mpu6050
+#from Encoder import Encoder 
+
+motor = Motor()
