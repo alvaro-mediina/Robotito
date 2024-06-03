0aca5e57ebe25cf308b9e4509c52ce09e62616fe
fulgor2 <alvaro.mediina2003@gmail.com>
2024-06-03 12:29:46 -0300

    Encoder funcionando + Contador de vueltas y pulsos

--- src/main.py
+++ src/main.py
@@ -1,11 +1,43 @@
 import sys
-sys.path.append("../utils/")
-sys.path.append("../movement/")
 import time
+import RPi.GPIO as GPIO
+
+sys.path.append("./utils/")
+sys.path.append("./movement/")
+sys.path.append("./data/")
 
 
-from motor import Motor
-#from Mpu6060 import Mpu6050
-#from Encoder import Encoder 
+from encoder import Encoder
+#from motor import Motor
+from RPi_map import ENCODER_A0,ENCODER_A1,ENCODER_B0,ENCODER_B1
+#motor = Motor()
 
-motor = Motor()
+# Desactivar las advertencias de pines GPIO en uso
+GPIO.setwarnings(False)
+
+
+# Inicializar el encoder
+encoder = Encoder()
+
+while True:
+    try:
+        # Iniciar la cuenta de pulsos
+        encoder.iniciar_cuenta()
+        
+        # Mantener el programa corriendo para contar los pulsos
+        print("Presiona Ctrl+C para detener la cuenta.")
+        while True:
+            time.sleep(1)
+    
+    except KeyboardInterrupt:
+        # Detener la cuenta de pulsos
+        encoder.detener_cuenta()
+        
+        # Obtener la distancia recorrida
+        distancia1, distancia2 = encoder.obtener_distancia()
+        print("Distancia recorrida por la rueda del motor A (ENCODER_B1): {:.2f} mm".format(distancia1))
+        print("Distancia recorrida por la rueda del motor B (ENCODER_A0): {:.2f} mm".format(distancia2))
+        
+        # Limpiar los GPIOs
+        GPIO.cleanup()
+
